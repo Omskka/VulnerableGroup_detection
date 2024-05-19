37b9ddad9077c5d570583c50ec6eac78c57f5d77
Omskka <omermurat2312@gmail.com>
2024-05-19 19:00:14 +0300

    done

--- gui.py
+++ gui.py
@@ -3,38 +3,38 @@ from imports import *
 # Define base directory
 BASE_DIR = Path(__file__).resolve().parent
 
+
 # Define paths
-ASSETS_PATH = BASE_DIR / "assets" / "frame0"  # assets directory path
-VIDEO_DIRECTORY = BASE_DIR / "videos"  # 'videos' directory path
-MODEL_PATH = BASE_DIR / "3val_18.pt"  # YOLOv8 model path
-CLASSES_PATH = BASE_DIR / "classes.txt"  # 'classes' file path
+ASSETS_PATH = BASE_DIR / "assets"/"frame0"  # assets directory path
+VIDEO_DIRECTORY = BASE_DIR /"videos"  # 'videos' directory path
+MODEL_PATH = BASE_DIR / "best.pt"  # YOLOv8 model path
+CLASSES_PATH = BASE_DIR / "classes.txt" # 'classes' file path
 
 
 def relative_to_assets(path: str) -> Path:
-    return BASE_DIR / "assets" / "frame0" / path
+    return BASE_DIR /"assets"/"frame0"/path
 
 
+# Function to store video path
 def store_video_path():
     global video_path
     video_path = VIDEO_DIRECTORY / entry_1.get()
     train_model_and_play(video_path)
 
 
- # count the number of resets
-reset_counter = 0 
-
+# Default coordinates for zoning(Area1)
+reset_counter = 0 # count the number of resets
 def reset_entries():
     global reset_counter, X1, Y1, X2, Y2, X3, Y3, X4, Y4
-    # Default coordinates for zoning(Area1)
-    X1 = 0 #400  
-    Y1 = 0 #140
-    X2 = 0 #24
-    Y2 = 600 #477
-    X3 = 1500 #950  
-    Y3 = 600 #477
-    X4 = 1500 #650  
-    Y4 = 0 #140
-    reset_counter += 1
+    X1 = 400 #24
+    Y1 = 140
+    X2 = 24
+    Y2 = 477
+    X3 = 950 #900
+    Y3 = 477
+    X4 = 650 #900
+    Y4 = 140
+    reset_counter +=1
     # Show info messsage when reset button is pressed(counter > 1)
     if reset_counter > 1:
         messagebox.showinfo("info", "The entry values have been reverted to their default settings.")
@@ -43,25 +43,23 @@ def reset_entries():
 # Initialise default values at program startup
 reset_entries()
 
+
 # Create window
 window = Tk()
 window.geometry("1000x600")
-window.configure(bg="#F5F4F4")
-
-# set default toggle to 'off'
-toggle_var = BooleanVar(value=False)
+window.configure(bg = "#F5F4F4")
 
 canvas = Canvas(
     window,
-    bg="#F5F4F4",
-    height=600,
-    width=1000,
-    bd=0,
-    highlightthickness=0,
-    relief="ridge"
+    bg = "#F5F4F4",
+    height = 600,
+    width = 1000,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
 )
 
-canvas.place(x=0, y=0)
+canvas.place(x = 0, y = 0)
 canvas.create_rectangle(
     0.0,
     0.0,
@@ -91,9 +89,7 @@ canvas.create_text(
     40.0,
     230.0,
     anchor="nw",
-    text="Discover a groundbreaking solution for\n enhanced accessibility through our app,\n harnessing the "
-         "cutting-edge YOLOV\n technology.\n\n By accurately detecting pedestrians,\n including wheelchair users, "
-         "it redefines\n safety and inclusivity, revolutionizing\n navigation experiences for all.",
+    text="Discover a groundbreaking solution for\n enhanced accessibility through our app,\n harnessing the cutting-edge YOLOV\n technology.\n\n By accurately detecting pedestrians,\n including wheelchair users, it redefines\n safety and inclusivity, revolutionizing\n navigation experiences for all.",
     fill="#FFFFFF",
     font=("Inter", 20 * -1)
 )
@@ -118,8 +114,7 @@ canvas.create_text(
 
 # Button_1
 button_image_1 = PhotoImage(
-    # Change "button_1.png" to "button_2.png" for a rectangle button
-    file=relative_to_assets("button_2.png"))  
+    file=relative_to_assets("button_2.png")) # Change "button_1.png" to "button_2.png" for a rectangle button
 
 # Update the button command to call play_video function
 button_1 = Button(
@@ -199,7 +194,7 @@ entry_1.bind("<FocusIn>", on_entry_click)
 # Create a label for the hyperlink
 info_label = Label(
     window,
-    text="For configuration, click here",
+    text="For zone configuration, click here",
     fg="white",  # Set text color to blue
     bg="#387EF5",  # Set background color to the same as the blue background
     cursor="hand2",  # Change cursor to a hand when hovering over the label
@@ -240,28 +235,27 @@ def validate_entries():
     # Restore the window after video ends
     window.deiconify()
 
-
 def config_window(event):
     # Minimize the window
     window.iconify()
 
     # Create window
-    global conf_window ,entry1, entry2, entry3, entry4, entry5, entry6, entry7, entry8, toggle_var
-    conf_window = Toplevel(window)
+    global conf_window
+    conf_window= Tk()
     conf_window.geometry("1000x600")
-    conf_window.configure(bg="#F5F4F4")
+    conf_window.configure(bg = "#F5F4F4")
 
     canvas2 = Canvas(
         conf_window,
-        bg="#F5F4F4",
-        height=600,
-        width=1000,
-        bd=0,
-        highlightthickness=0,
-        relief="ridge"
+        bg = "#F5F4F4",
+        height = 600,
+        width = 1000,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge"
     )
 
-    canvas2.place(x=0, y=0)
+    canvas2.place(x = 0, y = 0)
     canvas2.create_rectangle(
         0.0,
         0.0,
@@ -293,12 +287,12 @@ def config_window(event):
     done_button = Button(
         conf_window,
         text="Done",
-        command=validate_entries,
+        command = validate_entries,
         bg="#F5F4F4",  # Set background color
-        width=15,  # Set width (in characters)
-        height=2  # Set height (in lines)
+        width=15,      # Set width (in characters)
+        height=2       # Set height (in lines)
     )
-    done_button.place(x=660, y=300)
+    done_button.place(x=650, y=300)
 
     # Back button
     back_button = Button(
@@ -306,10 +300,10 @@ def config_window(event):
         text="Back",
         command=go_back,
         bg="#F5F4F4",  # Set background color
-        width=15,  # Set width (in characters)
-        height=2  # Set height (in lines)
+        width=15,      # Set width (in characters)
+        height=2       # Set height (in lines)
     )
-    back_button.place(x=660, y=360)
+    back_button.place(x=650, y=360)
 
     # Reset button
     back_button = Button(
@@ -317,8 +311,8 @@ def config_window(event):
         text="Reset",
         command=reset_entries,
         bg="#F5F4F4",  # Set background color
-        width=15,  # Set width (in characters)
-        height=2  # Set height (in lines)
+        width=15,      # Set width (in characters)
+        height=2       # Set height (in lines)
     )
     back_button.place(x=210, y=525)
 
@@ -342,48 +336,36 @@ def config_window(event):
     label_y += label_spacing
     Label(conf_window, text="Y4", bg="#387EF5", fg="#FFFFFF", font=("Inter", 16, "bold")).place(x=label_x, y=label_y)
 
-    # Entries
+    #Entries
     entry_x = 200
     entry_y = 40
     spacing = 60
 
+    global entry1, entry2, entry3, entry4, entry5, entry6, entry7, entry8
+
     entry1 = Entry(conf_window)
-    entry1.place(x=entry_x, y=entry_y)
+    entry1.place(x = entry_x, y = entry_y)
     entry_y += spacing
     entry2 = Entry(conf_window)
-    entry2.place(x=entry_x, y=entry_y)
+    entry2.place(x = entry_x, y = entry_y)
     entry_y += spacing
     entry3 = Entry(conf_window)
-    entry3.place(x=entry_x, y=entry_y)
+    entry3.place(x = entry_x, y = entry_y)
     entry_y += spacing
     entry4 = Entry(conf_window)
-    entry4.place(x=entry_x, y=entry_y)
+    entry4.place(x = entry_x, y = entry_y)
     entry_y += spacing
     entry5 = Entry(conf_window)
-    entry5.place(x=entry_x, y=entry_y)
+    entry5.place(x = entry_x, y = entry_y)
     entry_y += spacing
     entry6 = Entry(conf_window)
-    entry6.place(x=entry_x, y=entry_y)
+    entry6.place(x = entry_x, y = entry_y)
     entry_y += spacing
     entry7 = Entry(conf_window)
-    entry7.place(x=entry_x, y=entry_y)
+    entry7.place(x = entry_x, y = entry_y)
     entry_y += spacing
     entry8 = Entry(conf_window)
-    entry8.place(x=entry_x, y=entry_y)
-
-    # Toggle button
-    toggle_var = BooleanVar()
-    toggle_button = Checkbutton(conf_window, text="Toggle On/Off", variable=toggle_var, bg="#F5F4F4", font=("Inter", 16))
-    toggle_button.place(x=850, y=550)
-
-    canvas2.create_text(
-    660.0,
-    553.0,
-    anchor="nw",
-    text="Traffic light visualization",
-    fill="#625F80",
-    font=("Inter ExtraBold", 15 * -1)
-    )   
+    entry8.place(x = entry_x, y = entry_y)
 
     # Calculate the center coordinates of the screen
     screen_width = conf_window.winfo_screenwidth()
@@ -397,219 +379,132 @@ def config_window(event):
     conf_window.resizable(False, False)
     conf_window.mainloop()
 
-# Global variables to track time and traffic light state
-green_time = 2
-yellow_time = 2
-red_time = 2
-extra_red_duration = 10
-extra_green_duration = 10
-total_time = green_time + yellow_time + red_time
-
-
-def draw_traffic_light(current_phase):
-    # Define colors
-    GREY = (127, 127, 127)
-    RED = (0, 0, 255)
-    YELLOW = (0, 255, 255)
-    GREEN = (0, 255, 0)
-
-    # OpenCV window for the traffic light
-    cv2.namedWindow('Traffic Light', cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_NORMAL)
-
-    global object_detected
-
-    # Coordinates for circles
-    red_light = (50, 70)
-    yellow_light = (50, 150)
-    green_light = (50, 230)
-
-    # Radius for circles
-    radius = 30
-
-    # Create a black image
-    traffic_light = np.zeros((300, 100, 3), dtype=np.uint8)
-
-    if current_phase < green_time:
-        # Green phase
-        cv2.circle(traffic_light, red_light, radius, GREY, -1)
-        cv2.circle(traffic_light, yellow_light, radius, GREY, -1)
-        cv2.circle(traffic_light, green_light, radius, GREEN, -1)
-
-    elif current_phase < green_time + yellow_time:
-        # Yellow phase
-        cv2.circle(traffic_light, red_light, radius, GREY, -1)
-        cv2.circle(traffic_light, yellow_light, radius, YELLOW, -1)
-        cv2.circle(traffic_light, green_light, radius, GREY, -1)
-    elif current_phase < green_time + yellow_time + red_time:
-        # Red phase
-        cv2.circle(traffic_light, red_light, radius, RED, -1)
-        cv2.circle(traffic_light, yellow_light, radius, GREY, -1)
-        cv2.circle(traffic_light, green_light, radius, GREY, -1)
-        if object_detected:
-            cv2.putText(traffic_light, "+10 secs", (10, 280), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-
-    x_coordinate = 1220
-    y_coordinate = 150
-
-    # Show the traffic light
-    cv2.imshow('Traffic Light', traffic_light)
-    # Move the window 
-    cv2.moveWindow('Traffic Light', x_coordinate, y_coordinate)
-    # Resize the window to 100x300
-    cv2.resizeWindow('Traffic Light', 100, 300)
-
-
 # Bind <Button-1> event to open_link function
 info_label.bind("<Button-1>", config_window)
 
-
 def train_model_and_play(video_path):
-    global green_time, extra_green_duration, red_time, extra_red_duration, total_time, toggle_var
+    model=YOLO(MODEL_PATH)
 
-    model = YOLO(MODEL_PATH)
+    def RGB(event, x, y, flags, param):
+        if event == cv2.EVENT_MOUSEMOVE :
+            point = [x, y]
+            #print(point)
 
-    # Read from webcam 
-    print(video_path)
-    if video_path == VIDEO_DIRECTORY/"live" or video_path ==VIDEO_DIRECTORY/"LIVE" or video_path ==VIDEO_DIRECTORY/"Live":
-        cap = cv2.VideoCapture(0)
+    #cv2.namedWindow('RGB')
+    #cv2.setMouseCallback('RGB', RGB)
+    cap=cv2.VideoCapture(str(video_path))
+    if not cap.isOpened():  # Check if the video file opened successfully
+        # Print warning message
+        messagebox.showerror("error", "Error: Could not open video file.\nPlease make sure the video is in the 'videos' directory.")
+        return 
 
-    # Read from videopath
-    else:
-        cap = cv2.VideoCapture(str(video_path))
-        if not cap.isOpened():
-            messagebox.showerror("error", "Error: Could not open video file.\nPlease make sure the video is in the 'videos' directory.")
-            return 
-    
     my_file = open(CLASSES_PATH, "r")
     data = my_file.read()
     class_list = data.split("\n")
+    #print(class_list)
 
     tracker = Tracker()
-    area1 = [(X1, Y1), (X2, Y2), (X3, Y3), (X4, Y4)]
+    area1=[(X1,Y1),(X2,Y2),(X3,Y3),(X4,Y4)] # Zone coordinates
+    area2=[(548,290),(600,496),(637,493),(574,288)]
 
-    global object_detected
-    object_detected = False
     paused = False
 
     # Minimize the window
     window.iconify()
 
-    # ID arrays
+    # Initialize the list to store IDs of detectables in the zone
+    # PS: Current model doesn't detect prams and crutches
     wheelchairs_in_zone = []
-    prams_in_zone = []
+    prams_in_zone = [] 
     crutches_in_zone = []
 
-    start_time = time.time()
-
     while True:
-        current_time = time.time()
-        elapsed_time = current_time - start_time
-        # Update total_time for when red/green light duration is changed
-        total_time = green_time + yellow_time + red_time
-
         if not paused:
             ret, frame = cap.read()
             if not ret:
                 break
 
-            frame = cv2.resize(frame, (1020, 500))
+            frame = cv2.resize(frame,(1020,500))
 
-            results = model.predict(frame, conf=0.52)
+            results = model.predict(frame)
             a = results[0].boxes.data
             px = pd.DataFrame(a).astype("float")
 
             list = []
-            # Extract confidence scores
-            confidence_scores = results[0].boxes.conf.cpu().numpy()  
-            for index, row in px.iterrows():
+            for index,row in px.iterrows():
                 x1 = int(row[0])
                 y1 = int(row[1])
                 x2 = int(row[2])
                 y2 = int(row[3])
-                d  = int(row[5])
-                # Get the corresponding confidence score
-                confidence_score = confidence_scores[index]  
+                d = int(row[5])
 
                 c = class_list[d]
-                if ('wheelchair' in c or 'crutches' in c or 'prams' in c):
+                if 'Wheel-chairs' in c:
                     list.append([x1, y1, x2, y2])
-
             bbox_idx = tracker.update(list)
-
-            wheelchairs_in_zone = []
-            crutches_in_zone = []
-            prams_in_zone = []
-
-            # Zip bbox_idx with confidence_scores
-            bbox_confidence_pairs = zip(bbox_idx, confidence_scores)
-
-            for bbox, confidence_score in bbox_confidence_pairs:
+            wheelchairs_in_zone = []  # Clear the list before updating
+            for bbox in bbox_idx:
                 x3, y3, x4, y4, id = bbox
                 result = cv2.pointPolygonTest(np.array(area1, np.int32), (x4, y4), False)
+                
+                # Convert confidence scores to Python floats with limited precision (3 decimal places)
+                confidence_scores = [f"{score:.3f}" for score in results[0].boxes.conf.cpu().numpy()]
 
+                # Green Light, Red Light
+                #print(result)
+                '''if result >= 0:
+                    print("-----RED-----")
+                else:
+                    print("-----GREEN-----") '''
+                
+                # Draw rectangle when wheelchair is detected within the zone
                 if result >= 0:
-                    # Increment counters based on object type
-                    for r in results:
-                        for c in r.boxes.cls:
-                            class_label = model.names[int(c)]
-                            # Increment counter based on class label
-                            if class_label == "wheelchair":
-                                wheelchairs_in_zone.append(id)
-                            elif class_label == "crutches":
-                                crutches_in_zone.append(id)
-                            elif class_label == "prams":
-                                prams_in_zone.append(id) 
-
-                    cv2.circle(frame, (x4, y4), 5, (0, 0, 255), -1)
+                    # Append the ID of the wheelchair detected in the zone to the list
+                    wheelchairs_in_zone.append(id)
+                    #cv2.circle(frame,(x4, y4), 5, (0, 0, 255), -1)
                     cv2.rectangle(frame, (x3, y3), (x4, y4), (255, 255, 255), 2)
 
-                    # Draw confidence score on the rectangle
-                    confidence_str = f"{confidence_score:.3f}"  # Convert confidence score to string
-                    cvzone.putTextRect(frame, confidence_str, (x3, y3), 1, 1)
+                    # Iterate over confidence scores and display each one
+                    for confidence_score in confidence_scores:
+                        # Use cvzone.putTextRect to draw the text on the frame
+                        cvzone.putTextRect(frame, confidence_score, (x3, y3), 1, 1)
 
-                    # If a wheelchair is detected(once), extend the duration of the red light.
-                    if not object_detected:
-                        # print("RED LIGHT EXTENDED FOR 10 SECS")
-                        red_time += extra_red_duration
-                    object_detected = True
+            cv2.polylines(frame, [np.array(area1,np.int32)], True, (0, 255, 0), 2)
 
-            cv2.polylines(frame, [np.array(area1, np.int32)], True, (0, 255, 0), 2)
+            #Resize the frame to the desired size (600x1000)
+            frame = cv2.resize(frame, (1000, 600))
 
-            frame = cv2.resize(frame, (800, 600))
-
-            # counter bar
+            # Create a blank image for displaying wheelchair count
             count_image = np.zeros((600, 200, 3), dtype=np.uint8)
-            count_image[:] = (245, 126, 56)
+            count_image[:] = (245, 126, 56)  # Set background color (BRG) 
+            # Wheelchairs
+            cv2.putText(count_image, f"Wheelchairs : {len(wheelchairs_in_zone)}", (13, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+            # Crutches
+            cv2.putText(count_image, f"Crutches : {len(crutches_in_zone)}", (13, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+            # Prams
+            cv2.putText(count_image, f"Prams : {len(prams_in_zone)}", (13, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
 
-            cv2.putText(count_image, f"Wheelchairs : {len(wheelchairs_in_zone)}", (13, 150), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.7, (255, 255, 255), 2)
-
-            cv2.putText(count_image, f"Crutches : {len(crutches_in_zone)}", (13, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
-                        (255, 255, 255), 2)
-
-            cv2.putText(count_image, f"Prams : {len(prams_in_zone)}", (13, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
-                        (255, 255, 255), 2)
-
+            # Concatenate the frame and count image horizontally
             display_frame = np.hstack((count_image, frame))
-            cv2.imshow('Video Player', display_frame)
 
-            # Draw traffic lights if button is toggled
-            current_phase = elapsed_time % total_time
-            if toggle_var.get():
-                draw_traffic_light(current_phase)
+            cv2.imshow('Video Player', display_frame)  # Display the frame
 
+            # Get the width and height of the resized frame
             frame_height, frame_width, _ = display_frame.shape
+
+            # Calculate the position to center the window
             screen_width = window.winfo_screenwidth()
             screen_height = window.winfo_screenheight()
             x_coordinate = int((screen_width - frame_width) / 2)
             y_coordinate = int((screen_height - frame_height) / 2)
 
+            # Move the OpenCV window to the center of the screen
             cv2.moveWindow('Video Player', x_coordinate, y_coordinate)
 
         key = cv2.waitKey(1)
-        if key == ord('q') or not ret:
+        if key == ord('q') or not ret:  # Quit if 'q' is pressed
             break
-        elif key == ord(' '):
+        elif key == ord(' '):  # Pause if spacebar is pressed
             paused = not paused
 
     cap.release()
